fc9908c440ac1b7e7b6e0b6e67385b9a52ca9dfb
João Mesquita <jolitas2005@gmail.com>
2024-10-14 17:55:13 +0100

    Classe Utilizador

---
+++ src/main/java/Equipa2/Incremento1/Utilizador.java
@@ -0,0 +1,122 @@
+package Equipa2.Incremento1;
+
+import java.util.UUID;
+
+/**
+ * Classe que representa um utilizador.
+ */
+public class Utilizador {
+    private String id;
+    private String nome;
+    private String email;
+    private String password;
+    private String morada;
+
+    /**
+     * Construtor que inicializa um novo utilizador com os dados fornecidos.
+     * O ID é gerado automaticamente e aleatoriamente.
+     *
+     * @param nome     Nome do utilizador.
+     * @param email    Email do utilizador.
+     * @param password Password do utilizador.
+     * @param morada   Morada do utilizador.
+     */
+    public Utilizador(String nome, String email, String password, String morada) {
+        id = UUID.randomUUID().toString();
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+        this.morada = morada;
+    }
+
+    /**
+     * Obtém o ID do utilizador.
+     *
+     * @return ID do utilizador.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Obtém o nome do utilizador.
+     *
+     * @return Nome do utilizador.
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Obtém o email do utilizador.
+     *
+     * @return Email do utilizador.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Obtém a password do utilizador.
+     *
+     * @return Password do utilizador.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Obtém a morada do utilizador.
+     *
+     * @return Morada do utilizador.
+     */
+    public String getMorada() {
+        return morada;
+    }
+
+    /**
+     * Define o nome do utilizador.
+     *
+     * @param nome Nome do utilizador.
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * Define o email do utilizador.
+     *
+     * @param email Email do utilizador.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Define a password do utilizador.
+     *
+     * @param password Password do utilizador.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Define a morada do utilizador.
+     *
+     * @param morada Morada do utilizador.
+     */
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    /**
+     * Retorna uma representação em string do utilizador.
+     *
+     * @return String que representa o utilizador.
+     */
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nome: " + nome + ", Email: " + email + ", Morada: " + morada;
+    }
+}
