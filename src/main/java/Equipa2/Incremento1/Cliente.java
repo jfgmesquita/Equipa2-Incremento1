d9f8b482568aa41418f844bf213b32c9e96f0437
Devysson Cardoso <devyssonsc@gmail.com>
2024-10-18 18:44:47 +0100

    Adicionei um array de Solicitacoes a classe Cliente

--- src/main/java/Equipa2/Incremento1/Cliente.java
+++ src/main/java/Equipa2/Incremento1/Cliente.java
@@ -1,25 +1,31 @@
 package Equipa2.Incremento1;
 
+import java.util.List;
+import java.util.ArrayList;
 
 public class Cliente extends Utilizador {
 	// private ArrayList<Solicitacao> servicos; 
 	private String formaDePagamento; // eventualmente substituir por ArrayList de formas de pagamento
+	private List<Solicitacao> solicitacoes;
 	
-	 public Cliente(String nome, String email, String password, String morada, String formaDePagamento) {
+	public Cliente(String nome, String email, String password, String morada, String formaDePagamento) {
 		super(nome, email, password, morada);
 		this.formaDePagamento = formaDePagamento;
-	 }
-	 
-	
+		solicitacoes = new ArrayList<Solicitacao>();
+	}
 	 
 	 // getters
-	 public String getFormaDePagamento() {
-	        return formaDePagamento;
-	    }
+	public String getFormaDePagamento() {
+	    return formaDePagamento;
+	}
+
+	public List<Solicitacao> getSolicitacoes(){
+		return solicitacoes;
+	}
 	 
 	 // setters
-	 public void setFormaDePagamento(String formaDePagamento) {
+	public void setFormaDePagamento(String formaDePagamento) {
 	        this.formaDePagamento = formaDePagamento;
-	    }
+	}
 	 
 }
