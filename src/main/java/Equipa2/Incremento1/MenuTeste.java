531fa3ce3f81795038e2a43041958728a34041e0
Devysson Cardoso <devyssonsc@gmail.com>
2024-10-18 18:54:13 +0100

    Cria uma classe com um main para testes do Gere

---
+++ src/main/java/Equipa2/Incremento1/MenuTeste.java
@@ -0,0 +1,27 @@
+package Equipa2.Incremento1;
+
+import java.util.Scanner;
+import java.util.UUID;
+
+public class MenuTeste {
+    public static void main(String[] args){
+        // Scanner input = new Scanner(System.in);
+        // Gere gere = new Gere();
+
+        // Cliente cliente = new Cliente("Alykhan", "alyjamal77@gmail.com", "aly", "boavista", "mbway");
+        // Profissional pro = new Profissional("Divaldo", "divaldodias@gmail.com", "divas", "lousada", "Pintura", 3, 15);
+
+        // gere.registarCliente(cliente);
+        // gere.registarProfissional(pro);
+
+        // gere.solicitarServico(cliente, pro, cliente.getMorada(), "15:30 28-10-2024");
+        // gere.imprimirTodasSolicitacao();
+
+        // UUID idUltima = gere.ultimaSolicitacao().getId();
+        // System.out.println(idUltima);
+        // gere.cancelarSolicitacao(idUltima);
+        // gere.imprimirTodasSolicitacao();
+
+        // input.close();
+    }
+}
