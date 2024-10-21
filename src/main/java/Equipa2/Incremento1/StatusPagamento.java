c48e55766b2a6aa746347ace1ecb6b26af1c5eee
João Mesquita <jolitas2005@gmail.com>
2024-10-21 23:15:30 +0100

    StatusPagamento - Autor: Thiago >> Co-authored-by: Thiago Rocha <mcrxdesigner@gmail.com> Co-authored-by: Joao Mesquita <jolitas2005@gmail.com>

---
+++ src/main/java/Equipa2/Incremento1/StatusPagamento.java
@@ -0,0 +1,16 @@
+package Equipa2.Incremento1;
+
+/**
+ * A enumeração StatusPagamento representa os possíveis estados de um pagamento.
+ */
+public enum StatusPagamento {
+    /**
+     * Indica que o pagamento está pendente.
+     */
+    PENDENTE,
+
+    /**
+     * Indica que o pagamento foi realizado.
+     */
+    PAGO
+}
