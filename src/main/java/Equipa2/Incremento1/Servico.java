c58ad087de68a46d65038b93bc18631e8c51d173
teathiago <mcrxdesigner@gmail.com>
2024-10-22 14:52:32 +0100

    Fix do erro no GereDados
    Faltava as anotações na classe Serviço e também o ID do utilizador não pode ser inicializado no construtor, porque quando a gente bota
    @GeneratedValue(strategy = GenerationType.UUID) o hibernate já gera automaticamente, se ter os dois dá conflito

--- src/main/java/Equipa2/Incremento1/Servico.java
+++ src/main/java/Equipa2/Incremento1/Servico.java
@@ -2,6 +2,7 @@ package Equipa2.Incremento1;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,20 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "Servico")
 public class Servico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String titulo;
+
     private String descricao;
     private LocalDateTime data;
     private double valorHora;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profissional_id")
     private Profissional profissional;
 }
