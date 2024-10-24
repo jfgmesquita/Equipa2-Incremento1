1409318583b80e31d301777da63343d38325a923
teathiago <mcrxdesigner@gmail.com>
2024-10-24 22:42:38 +0100

    Update Solicitacao.java

--- src/main/java/Equipa2/Incremento1/Solicitacao.java
+++ src/main/java/Equipa2/Incremento1/Solicitacao.java
@@ -34,8 +34,8 @@ public class Solicitacao {
     @Column(name = "morada")
     private String morada;
 
-	@OneToOne(cascade = CascadeType.ALL)
-	private Pagamento pagamento;
+	// @OneToOne(cascade = CascadeType.ALL)
+	// private Pagamento pagamento;
 
     @Column(name = "data")
     private LocalDateTime data;
@@ -49,7 +49,7 @@ public class Solicitacao {
      * @param data			Data e horas.
      */
     public Solicitacao(Cliente cliente, Profissional profissional, LocalDateTime data) {
-        id = UUID.randomUUID();
+        
         this.cliente = cliente;
         this.profissional = profissional;
         this.morada = cliente.getMorada();
